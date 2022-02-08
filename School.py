1ee726cd910feffa785979df450cd0c145daec63
Ori Darshan <77589259+oridarshan@users.noreply.github.com>
2022-02-08 16:56:35 +0200

    school class

---
+++ School.py
@@ -0,0 +1,25 @@
+from Classroom import Classroom
+import Personnel
+
+
+class School:
+    def __init__(self, name, principle: Personnel.Principle, budget: int):
+        self.name = name
+        self.principle = principle
+        self.budget = budget
+        self.classes = []
+        self.teachers = []
+
+    def add_class(self, new_class: Classroom):
+        self.classes.append(new_class)
+
+    def add_teacher(self, new_teacher: Personnel.Teacher):
+        self.teachers.append(new_teacher)
+
+    def budget_status(self):
+        bdg = self.budget
+        for teacher in self.teachers:
+            bdg -= teacher.salary
+        bdg -= self.principle.salary
+        return bdg
+    
